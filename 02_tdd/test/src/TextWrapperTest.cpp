9ce34095f80daaf521e685035414a118e6ceca8a
Wiktor <cudek.w@gmail.com>
2019-10-14 16:16:06 +0200

    [red] checking columns

--- 02_tdd/test/src/TextWrapperTest.cpp
+++ 02_tdd/test/src/TextWrapperTest.cpp
@@ -9,3 +9,9 @@ TEST(TextWrapper, CreateInstance) {
     auto wrapper = TextWrapper{};
 
 }
+
+TEST(TextWrapper, HasColumnsGetter){
+    auto wrapper = TextWrapper{};
+
+    EXPECT_EQ(10, wrapper.columns());
+}
