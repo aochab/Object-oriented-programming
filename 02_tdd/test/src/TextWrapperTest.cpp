0c379243f193cf12c36d699a2edab946d1f529fa
Adrian <ochabadrian1@gmail.com>
2019-10-14 16:59:08 +0200

    [red] add functionality to return empty text

--- 02_tdd/test/src/TextWrapperTest.cpp
+++ 02_tdd/test/src/TextWrapperTest.cpp
@@ -15,7 +15,14 @@ TEST(TextWrapper, HasColumnsGetter){
 }
 
 TEST(TextWrapper, HasColumnsGetterInConstructor){
-    auto wrapper = TextWrapper(2);
+    auto wrapper = TextWrapper{2};
 
     EXPECT_EQ(2,wrapper.columns());
+}
+
+TEST(TextWrapper, GetEmptyText){
+    auto wrapper = TextWrapper{};
+    std::string text = "";
+
+    EXPECT_EQ("",wrapper.result());
 }
